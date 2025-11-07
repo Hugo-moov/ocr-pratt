f1a599b3b3a770c0ecc9d96e33a7b306a99477f6
Hugo Boronat <hugboron@publicisgroupe.net>
2025-11-07 10:58:59 -0500

    Add LLM post-processing and model validation

--- ocr.py
+++ ocr.py
@@ -2,6 +2,13 @@ from PIL import Image
 import pytesseract
 import pandas as pd
 import os
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import json
+
+load_dotenv()
+api_key = os.getenv("AZURE_OPENAI_KEY")
 
 if __name__ == "__main__":
 
@@ -23,16 +30,63 @@ if __name__ == "__main__":
 
     # Get result as boxes
     boxes_output = pytesseract.image_to_boxes(img_0)
-    print(f'Boxes output:\n\n{boxes_output}')
+    # print(f'Boxes output:\n\n{boxes_output}')
 
     # Get result as dictionary
     dict_output = pytesseract.image_to_data(img_0, output_type=pytesseract.Output.DICT)
     df_output = pd.DataFrame(dict_output)
-    print(f'Dict output:\n\n{df_output}')
+    # print(f'Dict output:\n\n{df_output}')
 
     # Get result as XML
     xml_output = pytesseract.image_to_alto_xml(img_0)
-    print(f'XML output:\n\n{df_output}')
+    # print(f'XML output:\n\n{df_output}')
+
+    # LLM post processing
+    endpoint = os.getenv("AZURE_ENDPOINT")
+    key = os.getenv("AZURE_KEY")
+    model_name = os.getenv("AZURE_MODEL_NAME")
+    deployment = os.getenv("AZURE_MODEL_NAME")
+
+    subscription_key = os.getenv("AZURE_KEY")
+    api_version = os.getenv("AZURE_API_VERSION")
+
+    client = AzureOpenAI(
+        api_version=api_version,
+        azure_endpoint=endpoint,
+        api_key=subscription_key,
+    )
+
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant to structure data coming from an OCR process.",
+            },
+            {
+                "role": "user",
+                "content": f"What is the total amount for this transaction ? Output the result in the format of a valid python dictionary with fields invoice_number, total_amount: {string_output}",
+            }
+        ],
+        max_completion_tokens=13107,
+        temperature=0.1,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        model=deployment
+    )
+
+    result = response.choices[0].message.content
+    print(result)
+
+    # Output validation
+
+    class InvoiceResult(BaseModel):
+        invoice_number: str
+        total_amount: float
+
+    validated = InvoiceResult(**json.loads(result))
+    print(validated)
+
 
 
 
